0b36c8fd833d66eed20ed3c9b85586eccb00975e
Sherie Pearl De Paz <pearlguro48@gmail.com>
2024-09-05 15:19:48 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,35 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/students/create', function () {
+    $student = new Student();
+    $student->first_name = 'John';
+    $student->last_name = 'Doe';
+    $student->email = 'johndoe@example.com';
+    $student->save();
+    return 'Student Created!';
+});
+
+Route::get('students', function(){
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email', 'johndoe@example.com')->first();
+    $student->email = 'johndoe@example.com';
+    $student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/students/delete', function () {
+    $student = Student::where('email', 'johndoe@example.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
 
 /*
 |--------------------------------------------------------------------------
